dbffd80a4dcccea05abd91b7d35ae2f3ddeef86e
carl-halliburton <carl.j.halliburton@gmail.com>
2018-02-15 11:09:57 +1300

    Added  Class Calculate to manage a singal operation, this is also the first committ and includes a blank gui window and controler

---
+++ DoubledPennyCalculator/DoubledPennyCalculator/Calculate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubledPennyCalculator
+{
+    class Calculate
+    {
+        private string input;
+        private string output;
+        private int count;
+        private long numDays;
+        private long pennyTotal;
+        private double dollarValue;
+
+        public Calculate(long days) {
+            count = 0;
+            numDays = days;
+            pennyTotal = 0;
+        }
+
+        public void DoublePenny()
+        {
+            SetPennyTotal(GetPennyTotal() * GetPennyTotal());
+        }
+
+        public void SetPennyTotal(long newTotal)
+        {
+            pennyTotal = newTotal;
+        }
+
+        public long GetPennyTotal()
+        {
+            return pennyTotal;
+        }
+
+        public void SetOutPut()
+        {
+            
+        }
+
+        public String GetOutPut()
+        {
+            return output;
+        }
+
+        public void SetCount()
+        {
+
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void FormeatDollar()
+        {
+            output = Convert.ToDecimal(dollarValue).ToString("#,##0.00");
+        }
+    }
+}
